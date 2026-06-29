a10d210e1a3b76f3a75b449bba3116e060730bb9
alokyadav1806 <ay1810665@gmail.com>
2026-06-29 09:02:29 +0530

    Add files via upload

---
+++ Day5/strongnumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1,digit,sum=0;
+    cout<<"Enter the number:";
+    cin>>n;
+    int n1=n;
+    while(n>0){
+        digit=n%10;
+        for(int i=1;i<=digit;i++){
+            fact=fact*i;
+        }
+        sum=sum+fact;
+            n=n/10;
+          fact=1;
+         }
+    if(sum==n1){
+    cout<<"Strong number";
+ }
+    else
+    cout<<"Not a perfect number";
+}
