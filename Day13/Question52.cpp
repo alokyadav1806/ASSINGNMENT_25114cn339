04d691bb747cc65fe017fedaf985d66dfed7e4a8
alokyadav1806 <ay1810665@gmail.com>
2026-07-03 21:34:18 +0530

    Add files via upload

---
+++ Day13/Question52.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    int even = 0, odd = 0;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    cout << "Even elements = " << even << endl;
+    cout << "Odd elements = " << odd << endl;
+
+    return 0;
+}
