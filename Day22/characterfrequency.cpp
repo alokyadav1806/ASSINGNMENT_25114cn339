0cb65b173b2a94fccf960d8d9a6a4376fe2caccd
alokyadav1806 <ay1810665@gmail.com>
2026-07-07 08:05:51 +0530

    Add files via upload

---
+++ Day22/characterfrequency.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int freq[256] = {0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(int)str[i]]++;
+    }
+
+    cout << "Character Frequencies:\n";
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] != 0) {
+            cout << (char)i << " = " << freq[i] << endl;
+        }
+    }
+
+    return 0;
+}
