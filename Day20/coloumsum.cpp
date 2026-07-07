a1b54e9f4227944e02bbf5156a13cb3426d858af
alokyadav1806 <ay1810665@gmail.com>
2026-07-07 07:48:53 +0530

    Add files via upload

---
+++ Day20/coloumsum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    int A[10][10];
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Column-wise sums:\n";
+    for (int j = 0; j < cols; j++) {
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            sum += A[i][j];
+        }
+        cout << "Sum of Column " << j + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
